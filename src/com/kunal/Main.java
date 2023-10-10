1dd693fe81986cd5b1392e0732e3cd3554177d31
pavithra <pavithraspp24@gmail.com>
2023-10-10 22:45:15 +0530

    commit

---
+++ src/com/kunal/Main.java
@@ -0,0 +1,16 @@
+package com.kunal;
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args)
+
+     {
+        Scanner scan = new Scanner(System.in);
+         System.out.println(scan.nextInt());
+
+        }
+
+}
